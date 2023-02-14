dd16cb685fb412fb108fbc5dfc8b50890def8212
TheHackMachine <whistleblower.ryan@gmail.com>
2023-02-15 01:48:32 +0600

    update

---
+++ Quiz/js/questions.js
@@ -0,0 +1,80 @@
+let questions = [
+    {
+    numb: 1,
+    question: "With which country India has held a meeting of PIC under the Indus Waters Treaty?",
+    answer: "Pakistan",
+    options: [
+      "China",
+      "Japan",
+      "Pakistan",
+      "USA"
+    ]
+  },
+    {
+    numb: 2,
+    question: "India and which country has conducted the 1st ever joint naval patrolling & PASSEX exercise?",
+    answer: "Madagascar",
+    options: [
+      "Russia",
+      "Japan",
+      "Bahrain",
+      "Madagascar"
+    ]
+  },
+    {
+    numb: 3,
+    question: "	The World's first exclusive Ship Tunnel is slated to be constructed in which country?",
+    answer: "Norway",
+    options: [
+      "Singapore",
+      "Germany",
+      "Norway",
+      "Finland"
+    ]
+  },
+    {
+    numb: 4,
+    question: "India has supplied 2 MHCs to which country?",
+    answer: "Iran",
+    options: [
+      "Iran",
+      "Bangladesh",
+      "Japan",
+      "Nepal"
+    ]
+  },
+    {
+    numb: 5,
+    question: "India is working on a third joint satellite Mission with which nation?",
+    answer: "France",
+    options: [
+      "Japan",
+      "France",
+      "USA",
+      "UK"
+    ]
+  },
+  {
+    numb: 6,
+    question: "India Has Larger Economy than?",
+    answer: "Austria",
+    options: [
+      "Japan",
+      "Austria",
+      "USA",
+      "UK"
+    ]
+  },
+  
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
